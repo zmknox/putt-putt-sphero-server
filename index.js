d5a744a8740f9efa63e52286fd893c221c013a07
Zach Knox <zmknox@gmail.com>
2016-11-12 02:30:04 -0500

    I think I got rid of some returns or something just let me pull the freaking changes

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 /* This is not how you make a web server. Whatever man. */
-
-
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
@@ -14,7 +13,6 @@ app.use(express.static('public'));
 app.get('/css/bootstrap.min.css', function(req, res){
   res.sendfile('node_modules/bootstrap3/dist/css/bootstrap.min.css');
 });
-
 
 io.on('connection', function(socket){
     console.log("client connected");
