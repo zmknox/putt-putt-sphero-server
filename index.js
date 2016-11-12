5b6f148b591a4e04e6ee670a7a34ee8271a497da
Zach Knox <zmknox@gmail.com>
2016-11-12 01:24:00 -0500

    Make an awful webserver

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+/* This is not how you make a web server. Whatever man. */
 app.get('/', function(req, res){
   res.sendfile('index.html');
+});
+
+app.get('/style.css', function(req, res){
+  res.sendfile('style.css');
+});
+
+app.get('/favicon.ico', function(req, res){
+  res.sendfile('favicon.ico');
+});
+
+app.get('/script.js', function(req, res){
+  res.sendfile('script.js');
 });
 
 io.on('connection', function(socket){
