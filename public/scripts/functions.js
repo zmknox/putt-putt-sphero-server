02616ac4e998fcbfff97fbf2739a1709c52bc632
Eyad Hasan <ehasan3@gmu.edu>
2016-11-12 10:45:20 -0500

    Added code to capture accelerometer data, hopefully

--- public/scripts/functions.js
+++ public/scripts/functions.js
@@ -2,3 +2,20 @@ function Name(){
   name = document.getElementById('nameInput').value;
   console.log(name);
 }
+/**$(function(){
+  $( "button.button" ).bind( "tap", tapHandler );
+
+  function tapHandler( event ){
+    $( event.target ).addClass( "tap" );
+  }
+});**/
+
+function accCapture(){
+  window.ondevicemotion=function(event){
+    var accelerationX = event.accelerationIncludingGravity.x;
+    var accelerationY = event.accelerationIncludingGravity.y;
+    var accelerationZ = event.accelerationIncludingGravity.z;
+    document.getElementById("test").innerHTML= accelerationY;
+  }
+
+  };
