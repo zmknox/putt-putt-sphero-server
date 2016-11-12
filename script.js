519bfbca1a228f3bceed213d43e8c7db3032a6b3
Eyad Hasan <ehasan3@gmu.edu>
2016-11-12 01:14:43 -0500

    Front End Code

---
+++ script.js
@@ -0,0 +1,9 @@
+/** FUCK JAVASCRIPT **/
+$(".modal-fullscreen").on('show.bs.modal', function(){
+  setTimeout( function() {
+    $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
+  }, 0);
+});
+$(".modal-fullscreen").on('hidden.bs.modal',function(){
+  $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
+});
