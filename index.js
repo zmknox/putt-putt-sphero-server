e721a367a790a76ef979cf16fb2d444fb3a4c759
Eyad Hasan <ehasan3@gmu.edu>
2016-11-12 02:28:46 -0500

    Can't use 'catch' as a function name... psh

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var io = require('socket.io')(http);
 
 /* This is not how you make a web server. Whatever man. */
 
-
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
@@ -14,7 +13,6 @@ app.use(express.static('public'));
 app.get('/css/bootstrap.min.css', function(req, res){
   res.sendfile('node_modules/bootstrap3/dist/css/bootstrap.min.css');
 });
-
 
 io.on('connection', function(socket){
     console.log("client connected");
